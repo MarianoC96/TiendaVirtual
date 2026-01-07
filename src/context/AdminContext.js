1e58300b1c68086375e5e9e8188e1262548d48be
Hugo Yupanqui <hugo.yupanqui.g@gmail.com>
2026-01-07 01:15:59 -0500

    asdasdsad

--- src/context/AdminContext.js
+++
@@ -1,7 +0,0 @@
-import React, { createContext, useState, useContext } from 'react';
-
-const AdminContext = createContext();
-
-export const useAdmin = () => useContext(AdminContext);
-
-export const AdminContextDefinition = AdminContext; // Exporting for Provider use if needed separate
